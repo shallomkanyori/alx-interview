7a31ac40877567d5f429d8eb3dcda1ac3406ab10
Shallom Kanyori <shallomkanyori@gmail.com>
2024-01-15 12:36:51 +0000

    Add Python function to solve min operations
    Add a Python function that returns the minimum number of operations to
    result in exactly n characters in a file where the file starts with a
    single character and the text editor can execute only two operations:
    Copy All and Paste.

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Minimum operations
+
+Functions:
+    minOperations(n)
+"""
+
+
+def minOperations(n: int) -> int:
+    """Solves the minimum operations problem.
+
+    In a text file, there is a single character H. Your text editor can execute
+    only two operations in this file: Copy All and Paste. Given a number n,
+    calculate the fewest number of operations needed to result in exactly n H
+    characters in the file.
+    """
+
+    dp = [float('inf')] * (n + 1)
+    dp[1] = 0
+
+    for i in range(2, n + 1):
+        # Calculate min operations to i from its factors
+        for j in range(1, int(i**0.5) + 1):
+            if i % j == 0:
+                dp[i] = min(dp[i], dp[j] + i // j, dp[i // j] + j)
+
+    return int(dp[n])
